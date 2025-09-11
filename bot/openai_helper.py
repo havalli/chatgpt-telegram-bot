340f19fcfc21f64360bd909f06a6c37711b56bf0
havalli <havalli@users.noreply.github.com>
2025-09-11 11:34:04 +0200

    Update openai_helper.py

--- bot/openai_helper.py
+++ bot/openai_helper.py
@@ -635,6 +635,8 @@ class OpenAIHelper:
             return base * 31
         if self.config['model'] in GPT_4O_MODELS:
             return base * 31
+        if self.config['model'] in GPT_5_MODELS:
+            return base * 31
         elif self.config['model'] in O_MODELS:
             # https://platform.openai.com/docs/models#o1
             if self.config['model'] == "o1":
